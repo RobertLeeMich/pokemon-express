00f73154aa9e2ffe3a8aa33ebbc8b50fb7ae4856
Robert Michaud <guildwarsguy123@yahoo.com>
2023-09-05 15:45:38 -0400

    can see the pokemon JSON in the browser

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
-const jsxViewEngine = require('jsx-view-engine');
+const jsxViewEngine = require("jsx-view-engine");
+const pokemon = require("./models/pokemon");
 
-app.set('view engine', 'jsx');
-app.set('views', './views');
-app.engine('jsx', jsxViewEngine());
+app.set("view engine", "jsx");
+app.set("views", "./views");
+app.engine("jsx", jsxViewEngine());
 
-
+//INDEX
 app.get("/", (req, res) => {
-    res.send("Welcome to the Pokemon App!")
-  })
+  res.send("Welcome to the Pokemon App!");
+});
+
+//DISPLAYING POKEMON IN THE BROWSER IN JSON
+app.get("/pokemon", (req, res) => {
+    res.send(pokemon)
+})
 
 app.listen(PORT, () => {
-    console.log(`Listening on port: ${PORT}`);
-  });
+  console.log(`Listening on port: ${PORT}`);
+});
